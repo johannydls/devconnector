5fa197a10b8faa2285663cff4e0aefa190615f59
Johanny de Lucena Santos <johanny.dls@gmail.com>
2020-03-20 17:37:13 -0300

    Navbar e Landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React, { Fragment } from 'react';
 import './App.css';
 
+import { Navbar } from './components/layout/Navbar';
+import { Landing } from './components/layout/Landing';
+
 const App = () => {
   return (
     <Fragment>
-      <h1>Hello World</h1>
+      <Navbar />
+      <Landing />
     </Fragment>
   );
 }
