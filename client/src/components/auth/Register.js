c228e1b99c68751f85ffd5a186aefd48d9038438
Johanny de Lucena Santos <johanny.dls@gmail.com>
2020-04-09 17:30:58 -0300

    Request simples para cadastro de usuário

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react'
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export const Register = () => {
 
@@ -14,12 +15,30 @@ export const Register = () => {
 
   const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
       console.log('Passwords do not match');
     } else {
-      console.log(formData);
+      const newUser = {
+        name,
+        email,
+        password
+      };
+
+      try {
+        const config = {
+          headers: {
+            'Content-Type': 'Application/json'
+          }
+        }
+        const body = JSON.stringify(newUser);
+        const res = await axios.post('/api/users', body, config);
+        console.log(res.data);
+      } catch (error) {
+        console.error(error.response.data);
+      }
+
     }
   }
 
